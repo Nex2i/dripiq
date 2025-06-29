feecb00b3868527f27292970127fe90e79abde64
Ryan H <ryanzhutch@gmail.com>
2025-06-29 14:40:12 -0600

    Initialize server structure with Fastify, TypeScript, and essential configurations. Added environment variables, ESLint, Prettier, and Nodemon configurations. Implemented core modules for user and tenant management, authentication, and error handling. Included Swagger for API documentation and established a PostgreSQL database schema with Prisma. Comprehensive README documentation provided.

---
+++ server/src/app.ts
@@ -0,0 +1,59 @@
+import { join } from 'path';
+import Fastify from 'fastify';
+import AutoLoad from '@fastify/autoload';
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import fastifyCors from '@fastify/cors';
+import fastifyEnv from '@fastify/env';
+import fastifyHelmet from '@fastify/helmet';
+import { schemaErrorFormatter } from '@/utils/schemaErrorFormatter';
+import { CREDENTIALS } from '@/config';
+import { schema } from '@/utils/validateEnv';
+import '@/extensions';
+import { logger } from '@/libs/logger';
+import { globalErrorHandler } from '@/utils/globalErrorHandler';
+
+async function startServer() {
+  const app = Fastify({
+    schemaErrorFormatter,
+    ajv: {
+      customOptions: {
+        coerceTypes: false,
+        allErrors: true,
+      },
+      plugins: [],
+    },
+    logger: true,
+    trustProxy: true,
+  }).withTypeProvider<TypeBoxTypeProvider>();
+
+  // Initialize Error Handling
+  app.setErrorHandler(globalErrorHandler);
+
+  // Initialize Plugins
+  await app.register(fastifyEnv, { dotenv: true, schema });
+  await app.register(fastifyCors, {
+    origin: true,
+    credentials: CREDENTIALS === 'true',
+  });
+  await app.register(fastifyHelmet);
+
+  await app.register(AutoLoad, {
+    dir: join(__dirname, '/plugins'),
+    dirNameRoutePrefix: false,
+  });
+
+  // Initialize Routes - KEEP THIS AS THE LAST REGISTRED ITEM
+  await app.register(AutoLoad, {
+    dir: join(__dirname, '/routes'),
+    dirNameRoutePrefix: false,
+    options: { prefix: `/api` },
+  });
+
+  return app;
+}
+
+export default startServer;
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
---
+++ server/src/utils/validateEnv.ts
@@ -0,0 +1,6 @@
+import { Type } from '@sinclair/typebox';
+
+export const schema = Type.Object({
+  API_VERSION: Type.String(),
+  PORT: Type.String(),
+});
