da61d8ec08815e96a574ae2861528e80fe30adc9
Ryan H <ryanzhutch@gmail.com>
2025-06-29 21:38:54 -0600

    feat: establish dripiq_app schema and migrate tables
    - Created a new schema "dripiq_app" for organizing database tables.
    - Updated table definitions for "tenants", "user_tenants", and "users" to reside within the new schema.
    - Adjusted foreign key constraints to reference the new schema, ensuring data integrity.
    - Removed outdated migration scripts related to schema migration and fixes.

--- server/src/db/migrations/0000_init.sql
+++ server/src/db/migrations/0000_init.sql
@@ -1,11 +1,13 @@
-CREATE TABLE IF NOT EXISTS "tenants" (
+CREATE SCHEMA "dripiq_app";
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "dripiq_app"."tenants" (
 	"id" text PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "user_tenants" (
+CREATE TABLE IF NOT EXISTS "dripiq_app"."user_tenants" (
 	"id" text PRIMARY KEY NOT NULL,
 	"user_id" text NOT NULL,
 	"tenant_id" text NOT NULL,
@@ -15,7 +17,7 @@ CREATE TABLE IF NOT EXISTS "user_tenants" (
 	CONSTRAINT "user_tenants_user_id_tenant_id_unique" UNIQUE("user_id","tenant_id")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "users" (
+CREATE TABLE IF NOT EXISTS "dripiq_app"."users" (
 	"id" text PRIMARY KEY NOT NULL,
 	"supabase_id" text NOT NULL,
 	"email" text NOT NULL,
@@ -28,13 +30,13 @@ CREATE TABLE IF NOT EXISTS "users" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "user_tenants" ADD CONSTRAINT "user_tenants_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "dripiq_app"."user_tenants" ADD CONSTRAINT "user_tenants_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "dripiq_app"."users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "user_tenants" ADD CONSTRAINT "user_tenants_tenant_id_tenants_id_fk" FOREIGN KEY ("tenant_id") REFERENCES "public"."tenants"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "dripiq_app"."user_tenants" ADD CONSTRAINT "user_tenants_tenant_id_tenants_id_fk" FOREIGN KEY ("tenant_id") REFERENCES "dripiq_app"."tenants"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
